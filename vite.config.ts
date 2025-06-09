95e1c840c77ea93caa99228c138397a628ef6bba
johmag2 <johan.o.magnusson@me.com>
2025-06-09 14:08:02 +0200

    Attempt to fix launch

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
+/// <reference types="node" />
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [sveltekit()],
+		build: {
+			sourcemap: true,
+			target: 'esnext'
+		},
+		define: {
+			'import.meta.env.VITE_USE_MOCK_DATA': JSON.stringify(env.VITE_USE_MOCK_DATA)
+		}
+	};
 });
